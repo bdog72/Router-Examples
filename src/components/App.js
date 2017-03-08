f147ca2b6a0c9e4c1a1e0dcf3d697a04a0f236d3
Brian Bycynski <high5bri@hotmail.com>
2017-03-08 13:03:20 -0500

    finished 3rd example

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Router1 from './Router1'
 import Router2 from './Router2'
+import Router3 from './Router3'
 
 export default class App extends Component {
   render () {
@@ -8,6 +9,8 @@ export default class App extends Component {
       <Router1 />
       <hr />
       <Router2 />
+      <hr />
+      <Router3 />
       <hr />
     </div>
   }
